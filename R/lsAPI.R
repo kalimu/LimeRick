16c42aba1ded90e96ee7a838173e9b5bda9bd385
kalimu <kamil.wais@gmail.com>
2016-08-03 10:09:50 +0200

    initial verison of function for API calls

--- R/lsAPI.R
+++ R/lsAPI.R
@@ -1,5 +1,31 @@
+#' Function direclty calling the LimeSurvey API
+#' todo: documentation
+#' @export
 
 
-
-# todo: function for LimeSurvey API calls
 # todo: there may also be in the future a function for connecting with LS without API
+
+lsAPI = function(lsAPIurl,
+                 method,
+                 params
+                 ){
+
+
+    bodyJSON = list(method = method,
+                    id = " ",
+                    params = params
+                    )
+
+    bodyJSON = jsonlite::toJSON(bodyJSON, auto_unbox = TRUE)
+
+    apiResponse = httr::POST(lsAPIurl,
+                             httr::content_type_json(),
+                             body = bodyJSON
+                             )
+
+    apiResult = jsonlite::fromJSON(httr::content(apiResponse,
+                                   encoding = "UTF-8"))$result
+
+    apiResult
+
+}
