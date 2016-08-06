6d67d4839663e42ac221a2ec7012fe9c1d141d12
kalimu <kamil.wais@gmail.com>
2016-08-06 17:10:54 +0200

    lsSessionKey

--- R/lsSessionKey.R
+++ R/lsSessionKey.R
@@ -1,5 +1,9 @@
+#' Function getting and releasing LimeSurvey API session key
+#' todo: documentation
+#' @export
+#'
+#'
 
-# todo: function for getting and releasing API session key
 
 # there are many solutions for setting user&password like those in limer repo and
 # http://blog.revolutionanalytics.com/2015/11/how-to-store-and-use-authentication-details-with-r.html
@@ -8,3 +12,29 @@
 # has been set privileges in LS
 # Anyhow he will be allowed to do whatever he can with survey via API and without them the code will not be reproducible
 # but the API user must be set as a special user with limited privileges not a superadministrator (!)
+
+
+lsSessionKey = function(#lsAPIurl,
+                        action = "release",
+                        user = NULL,
+                        pass = NULL,
+                        ...
+                        ){
+
+    if (action == "release") {
+
+        lsAPI(LimeSurveyAPI, method = "release_session_key")
+
+    } else if (action == "get") {
+
+        params = list(admin = user, password = pass)
+
+        lsAPI(LimeSurveyAPI, method = "get_session_key", params)
+
+    }
+
+
+
+
+
+}
