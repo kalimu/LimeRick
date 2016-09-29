8c1c2cee3f8a03b2e2a5f3d9a6442c51d34bce56
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:16:46 +0200

    Update lsGetSurveySummaryTokenInvalid.R

--- R/lsGetSurveySummaryTokenInvalid.R
+++ R/lsGetSurveySummaryTokenInvalid.R
@@ -1,1 +1,18 @@
 #' @export
+
+#########################
+#lsGetSurveySummaryTokenInvalid
+#########################
+
+lsGetSurveySummaryTokenInvalid = function(lsAPIurl,
+                          sessionKey,
+                          surveyID
+                          )
+                              
+  {
+                  lsGetSurveySummary(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'token_invalid'
+                          )
+  }
