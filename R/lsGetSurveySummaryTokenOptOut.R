bbf8829d4ac95a84810ea6f705f8cfa4372e92e0
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:15:54 +0200

    Update lsGetSurveySummaryTokenOptOut.R

--- R/lsGetSurveySummaryTokenOptOut.R
+++ R/lsGetSurveySummaryTokenOptOut.R
@@ -1,1 +1,18 @@
 #' @export
+
+#########################
+#lsGetSurveySummaryTokenOptOut
+#########################
+
+lsGetSurveySummaryTokenOptOut = function(lsAPIurl,
+                          sessionKey,
+                          surveyID
+                          )
+                              
+  {
+                  lsGetSurveySummary(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'token_opted_out'
+                          )
+  }
