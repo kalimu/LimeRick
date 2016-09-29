76d5328d111d6a1b89cca493b322113b45562766
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:19:46 +0200

    Update lsGetQuestionProperties.R

--- R/lsGetQuestionProperties.R
+++ R/lsGetQuestionProperties.R
@@ -1,1 +1,29 @@
 #' @export
+
+#########################
+#lsGetQuestionProperties
+#beta
+#########################
+
+lsGetQuestionProperties = function(sessionKey,
+                          questionID,
+                          #questionProperty,
+                          languageCode = "en"
+                          ){
+                          
+              params = list(sSessionKey = sessionKey,
+                  iQuestionID = questionID,
+                  aQuestionSettings = c("available_answers","subquestions","attributes","attributes_lang","answeroptions","defaultvalue"),
+                  sLanguageCode = languageCode,
+                  )
+                                  
+                                  data2 = lsAPI(lsAPIurl, method = "get_question_properties", params)
+ 
+                                  #beta - 1D array only  
+  
+                                      df2 = read.csv(textConnection(data2),
+                                        encoding = "UTF-8",
+                                        stringsAsFactors = FALSE)
+  
+                                      df2
+                          }
