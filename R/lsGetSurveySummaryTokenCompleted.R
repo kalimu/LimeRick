17437fcc9a54596ed27eedd8bd0e1b37ad1e16d7
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:15:27 +0200

    Update lsGetSurveySummaryTokenCompleted.R

--- R/lsGetSurveySummaryTokenCompleted.R
+++ R/lsGetSurveySummaryTokenCompleted.R
@@ -1,1 +1,18 @@
 #' @export
+
+#########################
+#lsGetSurveySummaryTokenCompleted
+#########################
+
+lsGetSurveySummaryTokenCompleted = function(lsAPIurl,
+                          sessionKey,
+                          surveyID
+                          )
+                              
+  {
+                  lsGetSurveySummary(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'token_completed'
+                          )
+  }
