6038825e6d5d953e2f25c838d92e61b0588bc425
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:14:56 +0200

    Update lsGetSurveySummaryCompletedResponses.R

--- R/lsGetSurveySummaryCompletedResponses.R
+++ R/lsGetSurveySummaryCompletedResponses.R
@@ -1,1 +1,18 @@
 #' @export
+
+#########################
+#lsGetSurveySummaryCompletedResponses
+#########################
+
+lsGetSurveySummaryCompletedResponses = function(lsAPIurl,
+                          sessionKey,
+                          surveyID
+                          )
+                              
+  {
+                  lsGetSurveySummary(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'completed_responses'
+                          )
+  }
