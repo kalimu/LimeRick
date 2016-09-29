e2b2be3716f1fe9fa3fd55f40b8e2af5d28867ec
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:14:31 +0200

    Update lsGetSurveySummaryIncompleteResponses.R

--- R/lsGetSurveySummaryIncompleteResponses.R
+++ R/lsGetSurveySummaryIncompleteResponses.R
@@ -1,1 +1,18 @@
 #' @export
+
+#########################
+#lsGetSurveySummaryIncompleteResponses
+#########################
+
+lsGetSurveySummaryIncompleteResponses = function(lsAPIurl,
+                          sessionKey,
+                          surveyID
+                          )
+                              
+  {
+                  lsGetSurveySummary(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'incomplete_responses'
+                          )
+  }
