0b2eaa12c3e033056657593fa34410b90b4871ed
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:18:57 +0200

    Update lsGetSurveyProperties.R

--- R/lsGetSurveyProperties.R
+++ R/lsGetSurveyProperties.R
@@ -1,1 +1,30 @@
 #' @export
+
+#########################
+#lsGetSurveyProperties
+#########################
+
+lsGetSurveyProperties = function(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          surveySettings #mandatory option
+
+#settings option not known disclosed by API
+                                 
+                             ){
+
+    params = list(sSessionKey = sessionKey,
+                  iSurveyID = surveyID,
+                  aSurveySettings = surveySettings
+                  )
+
+    data = lsAPI(lsAPIurl, method = "get_survey_properties", params)
+
+    data = rawToChar(base64enc::base64decode(data))
+
+    df = read.csv(textConnection(data),
+             encoding = "UTF-8",
+             stringsAsFactors = FALSE)
+  
+    df
+}
