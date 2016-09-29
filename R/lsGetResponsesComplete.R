48dcab8996387fc68f56530c45f99af9924313c2
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:12:56 +0200

    Update lsGetResponsesComplete.R

--- R/lsGetResponsesComplete.R
+++ R/lsGetResponsesComplete.R
@@ -1,1 +1,33 @@
 #' @export
+
+#########################
+#lsGetResponsesComplete
+#########################
+
+lsGetResponsesComplete = function(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus,# = "all",
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          ){
+                          
+                          lsGetResponses(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus = "complete",
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          )
+                          
+                          }
