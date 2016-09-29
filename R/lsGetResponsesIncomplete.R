0094747bd951ac3bc6001de40b7469e8f2ff5b7f
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:13:20 +0200

    Update lsGetResponsesIncomplete.R

--- R/lsGetResponsesIncomplete.R
+++ R/lsGetResponsesIncomplete.R
@@ -1,1 +1,33 @@
 #' @export
+
+#########################
+#lsGetResponsesIncomplete
+#########################
+
+lsGetResponsesIncomplete = function(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus,# = "all",
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          ){
+                          
+                          lsGetResponses(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus = "incomplete",
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          )
+                          
+                          }
