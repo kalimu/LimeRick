a7d0f08cdbef029a5594e47af06d32de2efd2e36
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:18:32 +0200

    Update lsGetSurveySummary.R

--- R/lsGetSurveySummary.R
+++ R/lsGetSurveySummary.R
@@ -1,1 +1,35 @@
 #' @export
+
+#########################
+#lsGetSurveySummary
+#private
+#########################
+
+lsGetSurveySummary = function(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          statName = 'all'
+                             ){
+
+    params = list(sSessionKey = sessionKey,
+                  iSurveyID = surveyID,
+                  sStatName = statName
+                  )
+
+    data = lsAPI(lsAPIurl, method = "get_summary", params)
+
+    data = rawToChar(base64enc::base64decode(data))
+
+    if (statName=="all") 
+        {
+        df = read.csv(textConnection(data),
+             encoding = "UTF-8",
+             stringsAsFactors = FALSE)
+        df
+        }
+    else
+        {
+    data
+        }
+  
+}
