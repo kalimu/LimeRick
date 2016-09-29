0609d643af2a06069e1ab7bf6e225c790ad17b46
michaljakub <michal.pietal@gmail.com>
2016-09-29 14:12:33 +0200

    Update lsGetResponsesAll.R

--- R/lsGetResponsesAll.R
+++ R/lsGetResponsesAll.R
@@ -1,1 +1,33 @@
 #' @export
+
+#########################
+#lsGetResponsesAll
+#########################
+
+lsGetResponsesAll = function(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus,
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          ){
+                          
+                          lsGetResponses(lsAPIurl,
+                          sessionKey,
+                          surveyID,
+                          completionStatus = "all",
+                          documentType = "csv",
+                          languageCode = "en",
+                          headingType = "code",
+                          responseType = "long",
+                          fromResponseID = NULL,
+                          toResponseID = NULL,
+                          fields = NULL
+                          )
+                          
+                          }
